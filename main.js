2b8f80f0224873b60a85b867c6984ea0482daec9
GyuCastle <86282616+GyuCastle@users.noreply.github.com>
2021-11-16 18:36:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ navbarMenu.addEventListener("click", (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove("open");
     scrollIntoView(link);
 });
 
@@ -29,7 +30,32 @@ contactMeBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
 
+//home 투명도 조절
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", ()=>{
+        home.style.opacity = 1 - window.scrollY/homeHeight;
+});
+//햄버거 메뉴 클릭할 때
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", ()=>{
+    navbarMenu.classList.toggle("open");
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" })
 }
+
+// contact btn
+const homeMainHeight = document.querySelector("#home").getBoundingClientRect().height;
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+    if(window.scrollY/homeMainHeight > 0.5)
+        arrowUp.classList.add("visible");
+    else
+        arrowUp.classList.remove("visible");
+});
+arrowUp.addEventListener("click", () => {
+    scrollIntoView("#home");
+});
